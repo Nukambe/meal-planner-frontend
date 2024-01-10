e58f4a31de1dd0cc66fd6d270804c1cf6f065a3c
nukambe <chappellwesley@gmail.com>
2024-01-10 18:26:48 -0500

    display daily macros

--- src/app/week-day/week-day.component.ts
+++ src/app/week-day/week-day.component.ts
@@ -5,11 +5,13 @@ import { dayOfWeek } from 'meal-planner-types';
 import { Observable } from 'rxjs';
 import { DayOfWeekPipe } from '../pipes/day-of-week.pipe';
 import { PlannedMealComponent } from '../planned-meal/planned-meal.component';
+import { Meal } from 'meal-planner-types';
+import { NutrientPipe } from '../pipes/nutrient.pipe';
 
 @Component({
   selector: 'app-week-day',
   standalone: true,
-  imports: [CommonModule, DayOfWeekPipe, PlannedMealComponent],
+  imports: [CommonModule, DayOfWeekPipe, PlannedMealComponent, NutrientPipe],
   templateUrl: './week-day.component.html',
   styleUrl: './week-day.component.css',
 })
@@ -27,5 +29,37 @@ export class WeekDayComponent {
   getListHeight(items: number[]): string {
     if (!this.showMeals) return '0px';
     return (items.length + 1) * 100 + 'px';
+  }
+
+  getDailyMealIds() {
+    return this.mealPlanService.getMealIdsByDay(this.week, this.day);
+  }
+
+  getMeals(ids: number[]) {
+    return this.mealPlanService.getMealsByIds(ids);
+  }
+
+  getDailyMacros(meals: Meal[]) {
+    const macros = [
+      { name: 'Calories', amount: 0, unit: '' },
+      { name: 'Carbs', amount: 0, unit: 'g' },
+      { name: 'Fat', amount: 0, unit: 'g' },
+      { name: 'Protein', amount: 0, unit: 'g' },
+    ];
+
+    return meals.reduce((acc, meal) => {
+      acc[0].amount +=
+        meal.nutrients.find((nutrient) => nutrient.name === 'Calories')
+          ?.amount || 0;
+      acc[1].amount +=
+        meal.nutrients.find((nutrient) => nutrient.name === 'Carbohydrates')
+          ?.amount || 0;
+      acc[2].amount +=
+        meal.nutrients.find((nutrient) => nutrient.name === 'Fat')?.amount || 0;
+      acc[3].amount +=
+        meal.nutrients.find((nutrient) => nutrient.name === 'Protein')
+          ?.amount || 0;
+      return acc;
+    }, macros);
   }
 }
